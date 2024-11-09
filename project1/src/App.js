5201964efdd5861e89b3d62e030cb4150e25eabd
VARUNKUMAR-GOVINdARAJ <varunkumar.govindaraj@gmail.com>
2024-11-09 11:01:41 +0530

    saved

---
+++ project1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './Components.js/Login';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Login/>
+
+    </div>
+  );
+}
+
+export default App;
